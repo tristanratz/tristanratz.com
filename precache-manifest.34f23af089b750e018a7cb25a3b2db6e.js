74d8006b8fbccbe74ddad472511651ee1814cb34
tristanratz <tristanratz@users.noreply.github.com>
2021-02-05 22:15:04 +0000

    deploy: 3708e5d0595a241854713ef91b07a4430d81aba0

---
+++ precache-manifest.34f23af089b750e018a7cb25a3b2db6e.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91369656b42a2e9ffe3fd41620d55758",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c97add73acc938b319b",
+    "url": "/static/css/main.dcac9bc0.chunk.css"
+  },
+  {
+    "revision": "27dca057ec93b2d85165",
+    "url": "/static/js/2.09c37929.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/static/js/2.09c37929.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c97add73acc938b319b",
+    "url": "/static/js/main.084f67c7.chunk.js"
+  },
+  {
+    "revision": "e28779c0cabeae0fbb52",
+    "url": "/static/js/runtime-main.5a7c007c.js"
+  },
+  {
+    "revision": "9f4c0893cbbedd11d422da2116d460e5",
+    "url": "/static/media/Titelfilm.9f4c0893.m4v"
+  },
+  {
+    "revision": "3d0f1b69299693d6a0b75c74e7e8b578",
+    "url": "/static/media/montserrat-black-webfont.3d0f1b69.svg"
+  },
+  {
+    "revision": "633dc1ddd1f612b7af5be9a526c48eec",
+    "url": "/static/media/montserrat-black-webfont.633dc1dd.woff"
+  },
+  {
+    "revision": "637111951f257e0b5fd4b7fb21fcfa28",
+    "url": "/static/media/montserrat-black-webfont.63711195.ttf"
+  },
+  {
+    "revision": "a6bf570dc89519567db0d9eb10d7c8b5",
+    "url": "/static/media/montserrat-black-webfont.a6bf570d.woff2"
+  },
+  {
+    "revision": "ae15a8519ad2f639ea333e9528b1a57b",
+    "url": "/static/media/montserrat-black-webfont.ae15a851.eot"
+  },
+  {
+    "revision": "3f2c835f845c03bc81c662641542513c",
+    "url": "/static/media/montserrat-bold-webfont.3f2c835f.svg"
+  },
+  {
+    "revision": "4030d35a6d84635454cdcb25583e2c75",
+    "url": "/static/media/montserrat-bold-webfont.4030d35a.ttf"
+  },
+  {
+    "revision": "c6188f7810c76f0fde9c154dad8383c3",
+    "url": "/static/media/montserrat-bold-webfont.c6188f78.woff2"
+  },
+  {
+    "revision": "f3abddb5a3121e763016cb656f74d938",
+    "url": "/static/media/montserrat-bold-webfont.f3abddb5.eot"
+  },
+  {
+    "revision": "fba4470c35c61ca13db530de501f6157",
+    "url": "/static/media/montserrat-bold-webfont.fba4470c.woff"
+  },
+  {
+    "revision": "0ab379c7b4944a4b4b34faeb8b14ad14",
+    "url": "/static/media/montserrat-regular-webfont.0ab379c7.woff"
+  },
+  {
+    "revision": "35cc8b4bad52134f2cf3833e0af208fe",
+    "url": "/static/media/montserrat-regular-webfont.35cc8b4b.woff2"
+  },
+  {
+    "revision": "4dbda720b2a46ae982d79901b8fc3d9f",
+    "url": "/static/media/montserrat-regular-webfont.4dbda720.ttf"
+  },
+  {
+    "revision": "c6d64ae99fd2cc40816d0dffef070f31",
+    "url": "/static/media/montserrat-regular-webfont.c6d64ae9.svg"
+  },
+  {
+    "revision": "c99078f024c9d0c4c9ad48dbbacafe01",
+    "url": "/static/media/montserrat-regular-webfont.c99078f0.eot"
+  },
+  {
+    "revision": "241fbbb86f39fbf0b83f0e68450c73f4",
+    "url": "/static/media/tristan.241fbbb8.jpg"
+  }
+]);
